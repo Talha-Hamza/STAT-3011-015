92e4260ee5edc21739d0c6834f94ecde3a61cf03
Talha Hamza <hamza050@umn.edu>
2025-02-04 16:07:27 -0600

    week3 - commit 1

--- week3/week3.R
+++ week3/week3.R
@@ -18,25 +18,35 @@ table(m_new$Rating)
 # a) Make a histogram of Budget using hist() with breaks = 15.
 hist(m_new$Budget, breaks = 15)
 # b) Construct a boxplot() to observe the overall distribution of the Budget. 
-
+boxplot(m_new$Budget)
 # c) What is the shape of the distribution? Are there any outliers? 
+# Right skewed. Yes, there are outliers
 # d) Which of the plots (histogram or boxplot) is easier to identify outliers? 
+# boxplot
 
 # Step 4.
 # Use summary() and report the five-number summary of the overall budget.
- 
+summary(m_new$Budget) 
+
 # Step 5. 
 # a) Note that by default mean() and sd() command uses option na.rm=FALSE. (Check ?mean, ?sd) 
-# b) If a variable contains any missing values, we have to use mean() command with na.rm=TRUE. 
-# c) This option removes all missing values to calculate mean and standard deviation.
+mean(m_new$Budget, na.rm = TRUE)
+sd(m_new$Budget, na.rm = TRUE)
+#If a variable contains any missing values, we have to use mean() command with na.rm=TRUE. 
+#) This option removes all missing values to calculate mean and standard deviation.
 
 # Step 6. 
 # a) The budget for the movie Fast and Furious is 85 million dollars. 
 # How many standard deviations is this movie's budget away from the mean? 
+s <- sd(m_new$Budget, na.rm = TRUE)
+xbar <- mean(m_new$Budget, na.rm = TRUE)
+(85-xbar)/s
 # b) Do you think the result is unusually large? 
+# No, it is 0.5 std away
 
 # Step 7. 
 # a) Modify the following command to create a side-by-side boxplot of the Budget separated by Ratings. 
+boxplot(m_new$Budget~m_new$Rating, xlab="Ratings", ylab= "In Million USD", main="Budget")
 # b) Overall which Rating tends to have the highest budget? 
 # c) Specify the name of the statistic
 
