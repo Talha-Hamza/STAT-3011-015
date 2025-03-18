334b3ca40237b6644043948d8391a97a4a5bb6ef
Talha Hamza <hamza050@umn.edu>
2025-03-18 16:22:36 -0500

    week 8 added

--- week8/problem3.R
+++ week8/problem3.R
@@ -18,5 +18,46 @@ hist(x,
      xlim = c(0, 12))  # xlim sets the x-axis range from 0 to 12
 
 set.seed(1)
-sample(x, size = 5, replace = TRUE)
+mysample <- sample(x, size = 5, replace = TRUE)
 
+mean(mysample)
+sd(mysample)
+
+xbar_n5 <- replicate(1000, mean(sample(x, size = 5, replace = TRUE)))
+replicate(2, mean(sample(x, size=5, replace = TRUE)))
+
+
+hist(xbar_n5, 
+     main = "Sampling Distribution of Sample Mean (n = 5)", 
+     xlab = "Sample Mean", 
+     xlim = c(0, 12))
+
+
+xbar_n50 <- replicate(1000, mean(sample(x, size = 50, replace = TRUE)))
+xbar_n100 <- replicate(1000, mean(sample(x, size = 100, replace = TRUE)))
+xbar_n200 <- replicate(1000, mean(sample(x, size = 200, replace = TRUE)))
+
+par(mfrow = c(2, 2)) 
+
+hist(xbar_n5, 
+     main = "Sampling Distribution (n = 5)", 
+     xlab = "Sample Mean", 
+     xlim = c(0, 12))
+
+hist(xbar_n50, 
+     main = "Sampling Distribution (n = 50)", 
+     xlab = "Sample Mean", 
+     xlim = c(0, 12))
+
+hist(xbar_n100, 
+     main = "Sampling Distribution (n = 100)", 
+     xlab = "Sample Mean", 
+     xlim = c(0, 12))
+
+hist(xbar_n200, 
+     main = "Sampling Distribution (n = 200)", 
+     xlab = "Sample Mean", 
+     xlim = c(0, 12))
+
+# Reset the plot window to default
+par(mfrow = c(1, 1))
